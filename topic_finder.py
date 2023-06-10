f02607fbdb399c3897cb82deb8e16b014e6b8807
Anay Modi <anaym@umich.edu>
2023-06-10 00:37:58 -0400

    Big Company stuff

--- topic_finder.py
+++ topic_finder.py
@@ -1,16 +1,34 @@
 import spacy
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
+import csv
+import requests
 
 # Load the spaCy English language model with the pre-trained NER component
 nlp = spacy.load('en_core_web_sm')
 
+#Fetch Company Stock Abbreviation and Name
+def fetch_company_data():
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+    CSV_URL = 'https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=A4C7A6Z2GY0I40TS'
+    company_list = []
+    with requests.Session() as s:
+        download = s.get(CSV_URL)
+        decoded_content = download.content.decode('utf-8')
+        cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+        my_list = list(cr)
+        for row in my_list:
+            data = f"{row[0]}, {row[1]}"
+            company_list.append(data)
+    
+    return company_list
 # Function to analyze sentiment and extract the topic, company name, and stock abbreviation
 def analyze_sentiment(text, company_list):
     doc = nlp(text)
     topic = ''
     company = ''
     stock_abbreviation = ''
+
 
     # Extract named entities and their labels
     entities = [(ent.text, ent.label_) for ent in doc.ents]
@@ -48,6 +66,7 @@ def analyze_sentiment(text, company_list):
 
 # Example usage
 sentence = "Microsodt Corporation is launching a new product."
+company_list = fetch_company_data()
 topic, company, stock_abbreviation = analyze_sentiment(sentence, company_list)
 print("Topic:", topic)
 print("Company:", company)
--- vantage_api.py
+++ vantage_api.py
@@ -1,36 +1,15 @@
+import csv
 import requests
-import os
-VANTAGE_API_KEY = os.environ["VANTAGE_API_KEY"]
-api_key = VANTAGE_API_KEY  # Replace with your Alpha Vantage API key
 
-# Endpoint URL for listing status
-url = f'https://www.alphavantage.co/query?function=LISTING_STATUS&apikey={api_key}'
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+CSV_URL = 'https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=A4C7A6Z2GY0I40TS'
 
-# Send a GET request to the API
-response = requests.get(url)
-
-# Process the response
-companies = []
-
-if response.status_code == 200:
-    data = response.json()
-    companies_data = data['data']
-   
-    for company in companies_data:
-        symbol = company['symbol']
-        name = company['name']
-        exchange = company['exchange']
-       
-        company_info = {
-            'symbol': symbol,
-            'name': name,
-            'exchange': exchange
-        }
-       
-        companies.append(company_info)
-else:
-    print('Error occurred while retrieving company list.')
-
-# Print the company information
-for company in companies:
-    print(f"Symbol: {company['symbol']}, Name: {company['name']}, Exchange: {company['exchange']}")
+with requests.Session() as s:
+    download = s.get(CSV_URL)
+    decoded_content = download.content.decode('utf-8')
+    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+    my_list = list(cr)
+    company_list = []
+    for row in my_list:
+        data = f"{row[0]}, {row[1]}"
+        company_list.append(data)
